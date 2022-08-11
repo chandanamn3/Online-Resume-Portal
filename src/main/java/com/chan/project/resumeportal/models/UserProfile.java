913ba12503e3238223e904d0f2e06d641b0d4835
chandana <chandanamn07@gmail.com>
2022-08-11 15:21:02 +0530

    Added basic user Profile elements and added or displayed in template

--- src/main/java/com/chan/project/resumeportal/models/UserProfile.java
+++ src/main/java/com/chan/project/resumeportal/models/UserProfile.java
@@ -12,6 +12,12 @@ public class UserProfile {
     private String userName;
 	private int theme;
     private String summary;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String designation;
+    
 	public int getId() {
 		return id;
 	}
@@ -35,6 +41,36 @@ public class UserProfile {
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
 	}
    
 }
