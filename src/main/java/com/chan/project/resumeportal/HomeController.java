f3079826f06b092ce7d8a71834e8a6beded266f9
chandana <chandanamn07@gmail.com>
2022-08-11 09:52:39 +0530

    Added a profile view path +Thymeleaf

--- src/main/java/com/chan/project/resumeportal/HomeController.java
+++ src/main/java/com/chan/project/resumeportal/HomeController.java
@@ -1,9 +1,12 @@
 package com.chan.project.resumeportal;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 	@GetMapping("/")
   public String home()
@@ -16,6 +19,11 @@ public class HomeController {
 	  {
 		  return "edit page";
 	  }
-	
+	@GetMapping("/view/{userId}")
+	  public String view(@PathVariable String userId,Model model)
+	  {
+		 model.addAttribute("userId", userId);
+		  return "profile";
+	  }
 	
 }
