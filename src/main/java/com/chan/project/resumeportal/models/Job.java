3b7532dc3a6011293edc1b1e5a6d6c882d24999a
chandana <chandanamn07@gmail.com>
2022-08-11 17:36:43 +0530

    Added job dispaly template

--- src/main/java/com/chan/project/resumeportal/models/Job.java
+++ src/main/java/com/chan/project/resumeportal/models/Job.java
@@ -2,6 +2,9 @@ package com.chan.project.resumeportal.models;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table
@@ -13,7 +16,25 @@ public class Job {
     private String designation;
     private LocalDate startDate;
     private LocalDate endDate;
+    private boolean isCurrentJob;
+    @ElementCollection(targetClass=String.class)
+    private List<String> responsibilities = new ArrayList();
 
+    public List<String> getResponsibilities() {
+        return responsibilities;
+    }
+
+    public void setResponsibilities(List<String> responsibilities) {
+        this.responsibilities = responsibilities;
+    }
+
+    public boolean isCurrentJob() {
+        return isCurrentJob;
+    }
+
+    public void setCurrentJob(boolean currentJob) {
+        isCurrentJob = currentJob;
+    }
     @Override
     public String toString() {
         return "Job{" +
@@ -63,5 +84,12 @@ public class Job {
 
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
+    }
+    public String getFormattedStartDate() {
+        return startDate.format(DateTimeFormatter.ofPattern("MMM yyyy"));
+    }
+
+    public String getFormattedEndDate() {
+        return endDate.format(DateTimeFormatter.ofPattern("MMM yyyy"));
     }
 }
